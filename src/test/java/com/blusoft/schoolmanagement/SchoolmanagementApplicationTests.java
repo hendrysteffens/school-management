a7f1d8d6b3e582c648267b87e4ffddf8690b75f6
Hendry Steffens <hendry.steffens@ifood.com.br>
2022-08-27 14:18:57 -0300

    First commit

---
+++ src/test/java/com/blusoft/schoolmanagement/SchoolmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blusoft.schoolmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
